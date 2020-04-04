d9bded285e100bfbac04788801c5586f99c84b53
James <jameskarlsson1@gmail.com>
2020-04-04 21:11:51 +1100

    Fixed render bug

--- js/scene.js
+++ js/scene.js
@@ -23,10 +23,6 @@ export function initialiseScene() {
   renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
 
-  /*var stats = new Stats();
-    stats.showPanel( 0 );
-    document.body.appendChild( stats.dom); */
-
   var light = new THREE.DirectionalLight(0xffffff, 2);
   light.position
     .set(camera.position.x, camera.position.y + 500, camera.position.z + 500)
@@ -36,6 +32,7 @@ export function initialiseScene() {
   //Terrain setup
   var geometry = new THREE.PlaneBufferGeometry(2000, 2000, 256, 256);
   var material = new THREE.MeshLambertMaterial({ color: 0x3c3951 });
+
   var terrain = new THREE.Mesh(geometry, material);
   terrain.rotation.x = -Math.PI / 2;
   terrain.geometry.attributes.position.needsUpdate = true;
@@ -46,28 +43,13 @@ export function initialiseScene() {
   //Change the peak value for different sizes.
   var perlin = new Perlin();
   var peak = 60;
-  //var smoothing = 300; //Necessary for the perlin smoothing
-  //function refreshVertices(){
+
   var vertices = terrain.geometry.attributes.position.array;
   for (var i = 0; i <= vertices.length; i += 3) {
     vertices[i + 2] = peak * Math.random();
-
-    /*vertices[i+2] = peak * perlin.noise(
-				(vertices[i])/smoothing,
-				(vertices[i+1])/smoothing 
-			);*/
   }
-
-  //}
-  /*
-	int cols, rows;
-	int scl = 20; 
-	for (int x  = 0; x < cols; x++) {
-		for (int y = 0; y < rows; y++) {
-		
-		}
-	} 
-	*/
+  terrain.geometry.attributes.position.needsUpdate = true;
+  terrain.geometry.computeVertexNormals();
 
   // Start the update loop
   renderer.setAnimationLoop(update);
