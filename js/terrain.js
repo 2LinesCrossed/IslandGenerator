b4cb9f0bb5d0e7470c7e4856d05c67cd598dab96
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2020-05-30 18:28:54 +1000

    Added jiebin's stuff (to merge later)

--- js/terrain.js
+++ js/terrain.js
@@ -1,5 +1,6 @@
 import * as THREE from './lib/three.js';
 import { Perlin } from './lib/perlin.js';
+
 export function generateTerrain() {
   var geometry = new THREE.PlaneBufferGeometry(2000, 2000, 256, 256);
   var material = new THREE.MeshLambertMaterial({ color: 0x3c3951 });
