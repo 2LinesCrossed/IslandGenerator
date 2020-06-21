8fb4a183006823d52ba6f59447f102e2b191a53c
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2020-06-21 18:49:49 +1000

    Added seed + shaders only compile on page load

--- js/terrain.js
+++ js/terrain.js
@@ -3,13 +3,14 @@ import { Perlin } from './lib/perlin.js';
 import { buildGUI } from './gui.js';
 import * as scene from './scene.js';
 
+let myseed = 0;
+
 //Peak affects the maximum height of mountains. Has different effects depending on smoothing and frequency.
 var peak = 400;
 //Smoothing affects the distribution and size of the 'islands', ranging from archipelago to a zoomed in chunk of a landmass
 var smoothing = 100;
-export var myseed = Math.floor(1000 * Math.random());
 //Increasing frequency makes the mountains more varied and interesting. Decreasing flattens things out.
-var freq = 40;
+var freq = 10;
 //Terracing option to make the landscape into layers.
 var terrace = 1;
 //Flattens specific vertices to create a 'polygonal' look.
@@ -25,6 +26,92 @@ var colorArr = [
   [230, 230, 180, 300]
 ];
 
+const materialOnBeforeCompile = (shader) => {
+  // Vertex Shader
+
+  shader.vertexShader = shader.vertexShader.replace(
+    `#include <common>`,
+    `
+        #include <common>
+        varying float y;
+        `
+  );
+  shader.vertexShader = shader.vertexShader.replace(
+    `#include <begin_vertex>`,
+    `
+        #include <begin_vertex>
+        y = ( position.z + 0.1 ) * 5.0;
+        `
+  );
+
+  // Fragment Shader
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    `#include <common>`,
+    `
+        #include <common>
+        varying float y;
+        vec3 col;
+        `
+  );
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    `gl_FragColor = vec4( outgoingLight, diffuseColor.a );`,
+    `
+        ${colorInt(colorArr)}
+        outgoingLight *= col;
+        gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+        `
+  );
+};
+//Shader colour definition for each bands. It retrieves different GLSL vector 3 colours from the array.
+var colorLevel = function (color) {
+  return `col = vec3 ((${color[0]}.0 / 255.0), (${color[1]}.0 / 255.0), (${color[2]}.0 / 255.0));`;
+};
+
+var colorInt = function (colorArray) {
+  let s = ``;
+  let vn = 0;
+  for (var i = 0; i < colorArray.length; i++) {
+    vn += colorArray[i][3];
+    let vd = vn - colorArray[i][3];
+    if (i == 0) {
+      s += `if (y <= ${vn}.0){
+            ${colorLevel(colorArray[i])}
+          }
+          `;
+    } else if (i == colorArray.length - 1) {
+      s += `if (y > ${vd}.0){
+            ${colorLevel(colorArray[i])}
+          }
+          `;
+    } else {
+      s += `if (y > ${vd}.0 && y < ${vn}.0 ){
+            ${colorLevel(colorArray[i])}
+          }
+          `;
+    }
+  }
+  return s;
+};
+
+const flatMaterial = new THREE.MeshPhongMaterial({ flatShading: true });
+flatMaterial.onBeforeCompile = materialOnBeforeCompile;
+
+const normals = THREE.ImageUtils.loadTexture('./textures/sand2.jpg');
+const smoothMaterial = new THREE.MeshPhysicalMaterial({
+  normalMap: normals,
+  normalScale: new THREE.Vector2(0.1, 0.01),
+  roughness: 0.7,
+  clearcoatNormalMap: normals,
+  clearcoatNormalScale: new THREE.Vector2(0.9, 1),
+  clearcoatRoughness: 0.56,
+  clearcoat: 0.4,
+  emissive: 0xffffff,
+  emissiveIntensity: 0.1
+});
+smoothMaterial.onBeforeCompile = materialOnBeforeCompile;
+
 buildGUI((gui, folders) => {
   var params = {
     hillpeak: peak,
@@ -39,8 +126,7 @@ buildGUI((gui, folders) => {
     peak = val;
     updateTerrain();
   });
-  folders.terrain.add(params, 'randomseed', 0, 1000).onChange(function (val) {
-    //TODO: Make this one work
+  folders.terrain.add(params, 'randomseed').onChange(function (val) {
     myseed = val;
     updateTerrain();
   });
@@ -48,7 +134,7 @@ buildGUI((gui, folders) => {
     smoothing = val;
     updateTerrain();
   });
-  folders.terrain.add(params, 'frequency', 1, 100).onChange(function (val) {
+  folders.terrain.add(params, 'frequency', 1, 25).onChange(function (val) {
     freq = val;
     updateTerrain();
   });
@@ -72,93 +158,11 @@ export function generateTerrain() {
   );
 
   const tileAmt = 10;
-  const normals = THREE.ImageUtils.loadTexture('./textures/sand2.jpg');
   normals.wrapS = THREE.RepeatWrapping;
   normals.wrapT = THREE.RepeatWrapping;
   normals.repeat.set(tileAmt, tileAmt);
 
-  var material = flatshader
-    ? new THREE.MeshPhongMaterial({ flatShading: true })
-    : new THREE.MeshPhysicalMaterial({
-        normalMap: normals,
-        normalScale: new THREE.Vector2(0.1, 0.01),
-        roughness: 0.7,
-        clearcoatNormalMap: normals,
-        clearcoatNormalScale: new THREE.Vector2(0.9, 1),
-        clearcoatRoughness: 0.56,
-        clearcoat: 0.4,
-        emissive: 0xffffff,
-        emissiveIntensity: 0.1
-      });
-
-  material.onBeforeCompile = (shader) => {
-    // Vertex Shader
-
-    shader.vertexShader = shader.vertexShader.replace(
-      `#include <common>`,
-      `
-        #include <common>
-        varying float y;
-        `
-    );
-    shader.vertexShader = shader.vertexShader.replace(
-      `#include <begin_vertex>`,
-      `
-        #include <begin_vertex>
-        y = ( position.z + 0.1 ) * 5.0;
-        `
-    );
-
-    // Fragment Shader
-
-    shader.fragmentShader = shader.fragmentShader.replace(
-      `#include <common>`,
-      `
-        #include <common>
-        varying float y;
-        vec3 col;
-        `
-    );
-
-    shader.fragmentShader = shader.fragmentShader.replace(
-      `gl_FragColor = vec4( outgoingLight, diffuseColor.a );`,
-      `
-        ${colorInt(colorArr)}
-        outgoingLight *= col;
-        gl_FragColor = vec4( outgoingLight, diffuseColor.a );
-        `
-    );
-  };
-  //Shader colour definition for each bands. It retrieves different GLSL vector 3 colours from the array.
-  var colorLevel = function (color) {
-    return `col = vec3 ((${color[0]}.0 / 255.0), (${color[1]}.0 / 255.0), (${color[2]}.0 / 255.0));`;
-  };
-
-  var colorInt = function (colorArray) {
-    let s = ``;
-    let vn = 0;
-    for (var i = 0; i < colorArray.length; i++) {
-      vn += colorArray[i][3];
-      let vd = vn - colorArray[i][3];
-      if (i == 0) {
-        s += `if (y <= ${vn}.0){
-            ${colorLevel(colorArray[i])}
-          }
-          `;
-      } else if (i == colorArray.length - 1) {
-        s += `if (y > ${vd}.0){
-            ${colorLevel(colorArray[i])}
-          }
-          `;
-      } else {
-        s += `if (y > ${vd}.0 && y < ${vn}.0 ){
-            ${colorLevel(colorArray[i])}
-          }
-          `;
-      }
-    }
-    return s;
-  };
+  var material = flatshader ? flatMaterial : smoothMaterial;
 
   var terrain = new THREE.Mesh(geometry, material);
   terrain.rotation.x = -Math.PI / 2;
@@ -166,8 +170,10 @@ export function generateTerrain() {
   terrain.geometry.computeVertexNormals();
 
   //Change the peak value for different sizes.
-  var perlin = new Perlin();
+  var perlin = new Perlin(myseed);
   var vertices = terrain.geometry.attributes.position.array;
+  console.log(freq);
+
   //Algorithm for creating proper mountainous landscapes
   for (var i = 0; i <= vertices.length; i += 3) {
     let x = vertices[i];
@@ -178,7 +184,6 @@ export function generateTerrain() {
     let vert = 0;
     let vertdiv1 = 1;
     let vertdiv2 = 1;
-    let vs = [];
     let itt = 0;
     while (itt < freq) {
       vert += vertdiv1 * perlin.noise(nx * vertdiv2, ny * vertdiv2);
@@ -191,7 +196,6 @@ export function generateTerrain() {
 
     vertices[i + 2] = Math.ceil((peak * vert) / terrace) * terrace;
   }
-
   terrain.geometry.attributes.position.needsUpdate = true;
   terrain.geometry.computeVertexNormals();
   return terrain;
@@ -201,19 +205,3 @@ function updateTerrain() {
   const newTerrain = generateTerrain();
   scene.setTerrain(newTerrain);
 }
-
-/*void function getYPosition(float_x, float_z){
-  var y = perlin.noise((float_x + 100) / 15, (float_z+100) / 15, mySeed);
-  y *= 10;
-  y += dist(float_x, float_z, 0, 0) / 2;
-  return y;
-}
-*/
-//Reverse engineer that one https://www.openprocessing.org/sketch/816746 for this section!
-/* float getYHeight (float _x, float _z) {
-	float y = noise((_x+100) / 15, mySeed, (_z+100) / 15);
-	y *= 10;
-	y += dist(_x, _z, 0, 0) / 2;
-	return y;
-} 
-*/
--- js/water.js
+++ js/water.js
@@ -9,6 +9,63 @@ let frequency = 0.05;
 let amplitude = 0.5;
 let waterSpeed = 1;
 
+const albedo = THREE.ImageUtils.loadTexture('./textures/seawater.jpg');
+const normalMap = THREE.ImageUtils.loadTexture(
+  './textures/seawater_normals.jpg'
+);
+
+const material = new THREE.MeshPhysicalMaterial({
+  color: 0x1f3a4d,
+  map: albedo,
+  normalMap: normalMap,
+  normalScale: new THREE.Vector2(0.1, 0.001),
+  clearcoat: 0.9,
+  clearcoatMap: albedo,
+  clearcoatNormalMap: normalMap,
+  clearcoatNormalScale: new THREE.Vector2(0.9, 1),
+  clearcoatRoughness: 0.01,
+  transparent: true,
+  roughness: 0.01,
+  transparency: 0.23,
+  reflectivity: 1.7
+});
+
+const declarationsGLSL = 'uniform float time, frequency, amplitude;\n';
+
+// Transforms vertices to form a sine wave pattern, and calculates normals so lighting isn't screwed up.
+// This equation is used to calculate surface normals:
+// https://stackoverflow.com/questions/9577868/glsl-calculate-surface-normal
+const vertexGLSL = `
+    vec3 transformed = vec3(position);
+    float angle = (time + position.x) * frequency;
+    transformed.z += sin(angle) * amplitude;
+
+    objectNormal = normalize(vec3(-amplitude * frequency * cos(angle), 0.0, 1.0));
+    vNormal = normalMatrix * objectNormal;
+  `;
+
+material.onBeforeCompile = (shader) => {
+  shader.uniforms.time = { value: 0 };
+  shader.uniforms.frequency = {
+    value: frequency
+  };
+  shader.uniforms.amplitude = {
+    value: amplitude
+  };
+
+  // Add new declarations to top of existing shader code
+  shader.vertexShader = declarationsGLSL + shader.vertexShader;
+  // Replace dummy vertex shader code with custom code
+  // (see https://blog.mozvr.com/customizing-vertex-shaders/)
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    vertexGLSL
+  );
+
+  // Store reference to shader so that values can be changed later
+  waterObj.shader = shader;
+};
+
 buildGUI((gui, folders) => {
   var params = {
     frequency,
@@ -31,10 +88,6 @@ buildGUI((gui, folders) => {
 
 export function createWater() {
   const { scale } = globalParams;
-  const albedo = THREE.ImageUtils.loadTexture('./textures/seawater.jpg');
-  const normalMap = THREE.ImageUtils.loadTexture(
-    './textures/seawater_normals.jpg'
-  );
 
   const tileAmt = 4 * scale;
 
@@ -46,57 +99,6 @@ export function createWater() {
   normalMap.wrapT = THREE.RepeatWrapping;
   normalMap.repeat.set(tileAmt, tileAmt);
 
-  const material = new THREE.MeshPhysicalMaterial({
-    color: 0x1f3a4d,
-    map: albedo,
-    normalMap: normalMap,
-    normalScale: new THREE.Vector2(0.1, 0.001),
-    clearcoat: 0.9,
-    clearcoatMap: albedo,
-    clearcoatNormalMap: normalMap,
-    clearcoatNormalScale: new THREE.Vector2(0.9, 1),
-    clearcoatRoughness: 0.01,
-    transparent: true,
-    roughness: 0.01,
-    transparency: 0.23,
-    reflectivity: 1.7
-  });
-
-  const declarationsGLSL = 'uniform float time, frequency, amplitude;\n';
-
-  // Transforms vertices to form a sine wave pattern, and calculates normals so lighting isn't screwed up.
-  // This equation is used to calculate surface normals:
-  // https://stackoverflow.com/questions/9577868/glsl-calculate-surface-normal
-  const vertexGLSL = `
-    vec3 transformed = vec3(position);
-    float angle = (time + position.x) * frequency;
-    transformed.z += sin(angle) * amplitude;
-
-    objectNormal = normalize(vec3(-amplitude * frequency * cos(angle), 0.0, 1.0));
-    vNormal = normalMatrix * objectNormal;
-  `;
-
-  material.onBeforeCompile = (shader) => {
-    shader.uniforms.time = { value: 0 };
-    shader.uniforms.frequency = {
-      value: frequency
-    };
-    shader.uniforms.amplitude = {
-      value: amplitude
-    };
-
-    // Add new declarations to top of existing shader code
-    shader.vertexShader = declarationsGLSL + shader.vertexShader;
-    // Replace dummy vertex shader code with custom code
-    // (see https://blog.mozvr.com/customizing-vertex-shaders/)
-    shader.vertexShader = shader.vertexShader.replace(
-      '#include <begin_vertex>',
-      vertexGLSL
-    );
-
-    // Store reference to shader so that values can be changed later
-    waterObj.shader = shader;
-  };
   const plane = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(
       4000 * scale,
